43d8b3d0eacc59de9d475d2f55d8dc1cbb48e702
kesiyarachelj <kesiyarj@gmail.com>
2025-01-20 22:35:18 +0530

    caesar_cipher

--- caesarcipher.py
+++ caesarcipher.py
@@ -5,7 +5,7 @@ for ch in message:
     ordinal_ch=ord(ch)
     ordinal_ch+=key
     if ordinal_ch > ord('z'):
-        ordinal_ch=ord('a')+(ordinal_ch-ord('z'))
+        ordinal_ch=ord('a')+(ordinal_ch-ord('z')-1)
     new_ch=chr(ordinal_ch)
     ciphertext+=new_ch
 print(ciphertext)
@@ -17,7 +17,7 @@ for ch in ciphertext:
     ordinal_ch = ord(ch)
     ordinal_ch -= key
     if ordinal_ch < ord('a'):
-        ordinal_ch = ord('z') - (ord('a') - ordinal_ch)
+        ordinal_ch = ord('z') - (ord('a') - ordinal_ch-1)
     new_ch = chr(ordinal_ch)
     decrypted_message += new_ch
 
